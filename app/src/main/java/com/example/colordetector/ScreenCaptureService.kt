638653ac424e6e6f93eb30adb65930500dc1c93b
dulledlove <108100952+dulledlove@users.noreply.github.com>
2025-07-03 21:39:43 -0500

    Update ScreenCaptureService.kt

--- app/src/main/java/com/example/colordetector/ScreenCaptureService.kt
+++ app/src/main/java/com/example/colordetector/ScreenCaptureService.kt
@@ -14,6 +14,7 @@ import android.view.*
 import android.widget.*
 import androidx.core.app.NotificationCompat
 import java.nio.ByteBuffer
+import android.hardware.display.DisplayManager
 
 class ScreenCaptureService : Service() {
 
